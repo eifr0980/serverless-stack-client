3219e51c22b317493b0cd5da11cb533ba108e9c3
Ofir L <eifr0980@gmail.com>
2020-03-04 10:33:59 +0200

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "notes-app-uploads-eifr"
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://nn5lbol7ah.execute-api.eu-central-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_3C4dJsvAa",
+    APP_CLIENT_ID: "6p7j2s3opgmadjlvdrb9cplc4m",
+    IDENTITY_POOL_ID: "eu-central-1:141b5073-ba1b-4433-9e31-1a5b966984d6"
+  }
+};
