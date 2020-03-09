32fa5b35bce9a6cc56383f56a31c47c6dfb0b4be
Ofir L <eifr0980@gmail.com>
2020-03-09 13:15:55 +0200

    test

--- src/config.js
+++ src/config.js
@@ -36,7 +36,6 @@ const prod = {
 
 // Default to dev if not set
 const config = process.env.REACT_APP_STAGE === "prod" ? prod : dev;
-
 export default {
   // Add common config values here
   MAX_ATTACHMENT_SIZE: 5000000,
